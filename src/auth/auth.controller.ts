ff62ba320b06ce4314d6dd4e96ab6f2c666860c0
uzairwaheed1 <106030156+uzairwaheed1@users.noreply.github.com>
2025-07-25 18:03:32 +0500

    Implement JWT authentication with Passport in Auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,16 @@ export class AuthController {
     @Post('/signup')
     async signup(@Body() authCredentialDto: AuthCredentialDto): Promise<void>{
         return this.authService.signUp(authCredentialDto)
+    }
+
+    @Post('/signin')
+    async signin(@Body() authCredentialDto: AuthCredentialDto): Promise<{accessToken: string}> {
+        return this.authService.signIn(authCredentialDto);
+    }
+
+    @Post('/protected')
+    @UseGuards(AuthGuard())
+    test(@Req() req): string {
+        return `This is a protected route. Welcome ${req.user.username}`;
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,21 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepositary } from './users.repositary';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // ✅ ENTITY only here
-  providers: [UserRepositary, AuthService],    // ✅ Include custom repository here
+  imports: [
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'yourSecretKey', // Use env variable in production!
+      signOptions: { expiresIn: '1h' },
+    }),
+    TypeOrmModule.forFeature([User]),
+], // ✅ ENTITY only here
+  providers: [UserRepositary, AuthService, JwtStrategy],    // ✅ Include custom repository here
   controllers: [AuthController],
-  exports: [UserRepositary],                   // ✅ So it can be used in other modules
+  exports: [UserRepositary, JwtStrategy, PassportModule],                   // ✅ So it can be used in other modules
 })
 export class AuthModule {}
